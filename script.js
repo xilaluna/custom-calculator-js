d7a5c639f54e5ab497b786bf9424b835b2730d87
xiluna <xilaluna2@gmail.com>
2020-10-06 14:18:13 -0700

    final

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
-const usdInput = document.querySelector("#input-usd");
-const displayEUR = document.querySelector("#euro-eur");
-const displayJPY = document.querySelector("#yen-jpy");
-const displayGBP = document.querySelector("#pound-gbp");
+function convertCurrency() {
+  const usdInput = document.querySelector("#input-usd");
+  const displayEUR = document.querySelector("#euro-eur");
+  const displayJPY = document.querySelector("#yen-jpy");
+  const displayGBP = document.querySelector("#pound-gbp");
 
-function calculateEUR() {
-  const usd = parseFloat(usdInput.value);
+  const input = usdInput.value;
+
   const eurExchangeRate = 0.85;
-  let calculated = usd * eurExchangeRate;
-  displayEUR.innerHTML = `${calculated}`;
+  const jpyExchangeRate = 105.6;
+  const gbpExchangeRate = 0.78;
+
+  displayEUR.innerHTML = (input * eurExchangeRate).toFixed(2);
+  displayJPY.innerHTML = (input * jpyExchangeRate).toFixed(2);
+  displayGBP.innerHTML = (input * gbpExchangeRate).toFixed(2);
 }
+
+document.getElementById("input-usd").addEventListener("input", convertCurrency);
